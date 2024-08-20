8ba316d5bf8eb1b872b5d44a8303bc7edd35c620
Alokcsit <swainalokkumar25@gmail.com>
2024-08-20 20:58:50 +0530

    initial commit

--- src/InsertData.js
+++ src/InsertData.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import { Form ,Input,FormGroup } from 'reactstrap'
 function InsertData() {
   return (
     <div>
-       <form>
-        hello
-       </form>
+       <Form>
+      <FormGroup>
+        <Input type="text" name="text" id="exampleText" placeholder="Enter your name" />
+      </FormGroup>
+       </Form>
     </div>
   )
 }
